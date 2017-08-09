94ddc77f54cd3cb9e08bdbd5f692fdb2800afcae
v3nd3tt4 <pilopaokta@gmail.com>
2017-08-10 03:07:34 +0700

    template blade

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,10 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('template/header');
+		$this->load->view('template/sidebar');
+		$this->load->view('template/navbar');		
+		$this->load->view('template/edit_user');
+		$this->load->view('template/footer');
 	}
 }
---
+++ application/views/template/sidebar.php
@@ -0,0 +1,42 @@
+    <nav class="navbar-default navbar-static-side" role="navigation">
+        <div class="sidebar-collapse">
+            <ul class="nav metismenu" id="side-menu">
+                <li class="nav-header">
+                    <div class="dropdown profile-element"> <span>
+                            <img alt="image" class="img-circle" width="100px" src="<?=base_url()?>assets/images/user_fb.jpeg" />
+                             </span>
+                        <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                            <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold"></strong>
+                            </span> <span class="text-muted text-xs block">Karatek <b class="caret"></b></span> </span> </a>
+                        <ul class="dropdown-menu animated fadeInRight m-t-xs">
+                          
+                            <li><a href="<?=base_url()?>adminNew/home/logout">Logout</a></li>
+                        </ul>
+                    </div>
+                    <div class="logo-element">
+                        IN+
+                    </div>
+                </li>
+                    <li>
+                    <a href="<?=base_url()?>adminNew/home"><i class="fa fa-th-large"></i> <span class="nav-label">Artikel</span> <!-- <span class="fa arrow"></span> --></a>
+                    <!-- <ul class="nav nav-second-level">
+                        <li class="active"><a href="index.html">Dashboard FMC</a></li>
+                        <li><a href="reguler.html">Dashboard V.2</a></li>
+
+                    </ul> -->
+                </li>
+                <li>
+                    <a href="<?=base_url()?>adminNew/home/dosen"><i class="fa fa-graduation-cap" aria-hidden="true"></i> <span class="nav-label">Dosen</span><!-- <span class="fa arrow"></span> --></a>
+                    <!-- <ul class="nav nav-second-level collapse">
+                        <li><a href="transaction.html">Used Oil</a></li>
+                        <li><a href="newoil.html">New Oil, Fuel & Others</a></li>
+                        <li><a href="unitno.html">UnitNo History</a></li>
+                        <li><a href="mhealth.html">Machine Health</a></li>
+                        <li><a href="chealth.html">Component Health</a></li>
+                        <li><a href="mcomparation.html">Machine Comparation</a></li>
+                    </ul> -->
+                </li>
+                
+            </ul>
+        </div>
+    </nav>
